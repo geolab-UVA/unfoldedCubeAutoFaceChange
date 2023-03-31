7281cfde747bcbb66042cfddbee2936c3c488d38
Katherine Moore <123612286+katherine-moore@users.noreply.github.com>
2023-03-31 00:29:06 -0400

    Update cube-face-arranger.js

--- js/cube-face-arranger.js
+++ js/cube-face-arranger.js
@@ -1,7 +1,7 @@
 AFRAME.registerComponent("cube-face-arranger", {
   // https://aframe.io/docs/1.4.0/introduction/writing-a-component.html#defining-properties-with-the-schema
   schema: {
-    currentFace: { type: "int", default: 0 },
+    currentFace: { type: "int", default: 0 }
   },
 
   init: function () {
@@ -13,10 +13,7 @@ AFRAME.registerComponent("cube-face-arranger", {
     console.log(" with data ");
     console.log(this.data);
 
-    
     this.throttledDebug = AFRAME.utils.throttle(this.debug, 1000, this);
-
-
 
     // Initialize the varaible `currentFace` that stores the current face, passed through the component attribute
     // https://aframe.io/docs/1.4.0/introduction/writing-a-component.html
@@ -24,7 +21,6 @@ AFRAME.registerComponent("cube-face-arranger", {
 
     this.currentFace = this.data.currentFace;
     console.log("currentFace = " + this.currentFace);
-
 
     // Store references to the faces with id face_X (X from 0 to 5)
     /* The faces of the cube, in order from 0 to 5, are arranged as follows:
@@ -39,12 +35,11 @@ AFRAME.registerComponent("cube-face-arranger", {
     *********  *********  *********  *********
   */
 
-    this.faces = []; // This array (arrays are indexed from 0) will store the faces. 
+    this.faces = []; // This array (arrays are indexed from 0) will store the faces.
 
     // We recover the faces by looking at children nodes of thes element to which the component is attached to
     // and looking for the ones with id face_X (X from 0 to 5)
     // In HTML the #string selector selects the element with id attribute `string`
-    
 
     for (const facePosition of [0, 1, 2, 3, 4, 5]) {
       this.faces[facePosition] = this.el.querySelector("#face_" + facePosition);
@@ -54,34 +49,107 @@ AFRAME.registerComponent("cube-face-arranger", {
     console.log("faces = ");
     console.log(this.faces);
 
-    this.arrangeFaces(); // Call function 
+    this.arrangeFaces(); // Call function
   },
 
   arrangeFaces: function () {
     // Here you should perform operations to correctly arrange the faces
 
-    
+    this.faces[0].object3D.position.x = 0;
+    this.faces[0].object3D.position.z = 0;
+
+    this.faces[1].object3D.position.x = 10;
+    this.faces[1].object3D.position.z = 0;
+
+    this.faces[2].object3D.position.x = 0;
+    this.faces[2].object3D.position.z = -10;
+
+    this.faces[3].object3D.position.x = -10;
+    this.faces[3].object3D.position.z = 0;
+
+    this.faces[4].object3D.position.x = 0;
+    this.faces[4].object3D.position.z = 10;
+
+    this.faces[5].object3D.position.x = 20;
+    this.faces[5].object3D.position.z = 0;
+
     // Here are some examples:
     // This changes the x coordinate of face 1 to 10
-    this.faces[1].object3D.position.x=10;
-    
+    //this.faces[1].object3D.position.x = 10;
+
     // Rotates the face the face by 45 degrees
-    this.faces[1].object3D.rotation.y=Math.PI/4;
-    
+    //this.faces[1].object3D.rotation.y = Math.PI / 4;
 
-
-    // Here is a switch statement that 
-    switch(this.currentFace) {
+    // Here is a switch statement that
+    switch (this.currentFace) {
       case 0:
+        this.faces[0].object3D.position.x = 0;
+        this.faces[0].object3D.position.z = 0;
+
+        this.faces[1].object3D.position.x = 10;
+        this.faces[1].object3D.position.z = 0;
+
+        this.faces[2].object3D.position.x = 0;
+        this.faces[2].object3D.position.z = -10;
+
+        this.faces[3].object3D.position.x = -10;
+        this.faces[3].object3D.position.z = 0;
+
+        this.faces[4].object3D.position.x = 0;
+        this.faces[4].object3D.position.z = 10;
+
+        this.faces[5].object3D.position.x = 20;
+        this.faces[5].object3D.position.z = 0;
+
         console.log("HERE YOU SHOULD PUT CODE THAT DEALS WITH currentFace = 0");
         break;
+
       case 1:
-        console.log("HERE YOU SHOULD PUT CODE THAT DEALS WITH currentFace = 1");
+        this.faces[0].object3D.position.x = -10;
+        this.faces[0].object3D.position.z = 0;
+
+        this.faces[1].object3D.position.x = 0;
+        this.faces[1].object3D.position.z = 0;
+
+        this.faces[2].object3D.position.x = 0;
+        this.faces[2].object3D.position.z = -10;
+
+        this.faces[3].object3D.position.x = 20;
+        this.faces[3].object3D.position.z = 0;
+
+        this.faces[4].object3D.position.x = 0;
+        this.faces[4].object3D.position.z = 10;
+
+        this.faces[5].object3D.position.x = 10;
+        this.faces[5].object3D.position.z = 0;
+
+        console.log("currentFace = 1");
         break;
-     }
+
+      case 2:
+        //this.faces[2].object3D.position.x = 0;
+        //this.faces[2].object3D.position.z = -10;
+        console.log("NOT YET IMPLEMENTED");
+        break;
+      case 3:
+        //this.faces[3].object3D.position.x = -10;
+        //this.faces[3].object3D.position.z = 0;
+        console.log("NOT YET IMPLEMENTED");
+        break;
+      case 4:
+        //this.faces[4].object3D.position.x = 0;
+        //this.faces[4].object3D.position.z = 10;
+        console.log("NOT YET IMPLEMENTED");
+        break;
+      case 5:
+        //this.faces[5].object3D.position.x = 20;
+        //this.faces[5].object3D.position.z = 0;
+        console.log("NOT YET IMPLEMENTED");
+        break;
+    }
   },
 
-  tick: function (t, dt) { },
+  tick: function (t, dt) {},
 
   /**
    * This is a debugging function
@@ -102,5 +170,5 @@ AFRAME.registerComponent("cube-face-arranger", {
         console.log(msg);
       }
     }
-  },
+  }
 });
